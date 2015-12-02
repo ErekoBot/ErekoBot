49f911edb946e1435a68dfd3a7828ec6d5fa79bd
samuelvenzi <samuel.venzi@me.com>
2015-12-02 15:34:01 -0200

    Arquivos para teste e estudo do IMU.

---
+++ tests/IMU/6DOF/sixDOF_Example_with_IR_conditional/sixDOF_Example_with_IR_conditional.ino
@@ -0,0 +1,39 @@
+#include <FreeSixIMU.h>
+#include <FIMU_ADXL345.h>
+#include <FIMU_ITG3200.h>
+
+#include <Wire.h>
+
+float angles[3]; // yaw pitch roll
+
+// Set the FreeSixIMU object
+FreeSixIMU sixDOF = FreeSixIMU();
+
+void setup() { 
+  Serial.begin(9600);
+  Wire.begin();
+  
+  delay(5);
+  sixDOF.init(); //begin the IMU
+  delay(5);
+}
+
+void loop() { 
+  
+  sixDOF.getEuler(angles);
+  
+  Serial.print(angles[0]);
+  Serial.print(" | ");  
+  Serial.print(angles[1]);
+  Serial.print(" | ");
+  Serial.println(angles[2]);
+  
+  if(angles[1] > -10 && angles[1] < 10){
+    
+    delay(5000);
+  
+  }
+  
+  delay(100); 
+}
+
--- tests/test_using_ir_servo/test_using_ir_servo.ino
+++ tests/test_using_ir_servo/test_using_ir_servo.ino
@@ -1,19 +1,23 @@
+#include <DistanceSensor.h>
+#include <UltrasonicDistanceSensor.h>
+#include <AnalogDistanceSensor.h>
 #include <DistanceGP2Y0A21YK.h>
+#include <DistanceGP2Y0A41SK.h>
+#include <DistanceSRF04.h>
+
 #include <Servo.h>
 #include <Wire.h>
 
 Servo myservo;
 char estado;
 float d0 = 35, d = 70 , pi = 3.141592, a = pi / 12, k = 1, anguloMotor, M = 5,  i = 2, psi = 0;
-DistanceGP2Y0A21YK Dist;
-int distance;
+
 
 
 void setup(){
   Serial.begin(9600);
   myservo.attach(9);            // conecta o servo no pino 9
-  Dist.begin(0);
-
+  DistanceGP2Y0A41YK Dist;
 } 
 void loop(){
 
@@ -24,15 +28,8 @@ void loop(){
       anguloMotor = (2 * a * sin(pi * k / M) * sin(psi + ((2 * pi * k / M) * (i - 1 + d0 / d))) * 180 / pi) + 90;    
       myservo.write(anguloMotor); 
       Serial.println(" em movimento ");
+      delay(20); 
       delay(20);
-      distance = Dist.getDistanceCentimeter();
-      Serial.print("\nDistance in centimers: ");
-      Serial.print(distance);  
-      delay(20);
-      if(distance <= 10||(Serial.read()=='p')){
-         estado =  'p';
-      }
-      
       if(psi == 2*pi){
         psi = 0;
       }else{
