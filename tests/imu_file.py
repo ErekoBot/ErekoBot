f8d46c09c77818ebb2d69836f83e2d184d4dae5a
samuelvenzi <samuel.venzi@me.com>
2016-03-17 16:38:02 -0300

    First version of program to create a file with IMU data

---
+++ tests/imu_file.py
@@ -0,0 +1,13 @@
+import serial
+
+s = serial.Serial('/dev/ttyUSB1', baudrate = 9600)
+
+f = open("imu_data.txt", "w")
+data_to_write = str(raw_input(">>"))
+s.write(data_to_write)
+while True:
+	line = s.read()
+	f.write(line)
+
+
+f.close()
