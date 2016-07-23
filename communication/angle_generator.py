df29b10b34a89f41474c5a545c6521852967653b
Samuel Venzi <samuel.venzi@me.com>
2016-07-23 20:14:28 -0300

    Profile 2 application development
    Need to find a way to stop movement sending data to Xbee.

--- communication/angle_generator.py
+++ communication/angle_generator.py
@@ -1,6 +1,5 @@
 import math
 
-
 d0 = 42
 d = 81
 pi = 3.141592
@@ -10,10 +9,16 @@ M = 5
 i = 1
 psi = 0
 flag = True
+text_file = open("angles.txt","w")
+
 while flag:
     angle = (2*a* math.sin(pi*k/M) * math.sin(psi+((2*pi*k/M)*(i-1+d0/d)))*180/pi) + 90
     print(angle)
+    angle = str(int(angle))
+    text_file.write(angle)
+    text_file.write(", ")
     if psi > 2*pi:
         flag = False
     psi += pi/180
 
+text_file.close()
--- communication/profile_2/main.py
+++ communication/profile_2/main.py
@@ -3,7 +3,7 @@ import math
 from time import sleep
 
 if __name__ == "__main__":
-    xbee = XBee.XBee("/dev/ttyUSB1")  # Your serial port name here
+    xbee = XBee.XBee("/dev/ttyUSB0")  # Your serial port name here
 
     while True:
         data_to_write = str(raw_input(">>"))
